c00fb5408105413ac01b66cd038e791f770e69be
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-20 14:47:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
 # your code goes here
+def begins_with_r(array)
+  array.all? do |word|
+    word[0] == "r"
+  end
+end
+
+def contain_a(array)
+  array.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.detect do |word|
+    word[0,2] == "wa"
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |word|
+    word.is_a?(String) == false
+  end
+end
