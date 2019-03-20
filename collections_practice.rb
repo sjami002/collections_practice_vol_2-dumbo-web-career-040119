69af36288eae1a3c7dcc15f53986276167601605
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-20 15:25:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,3 +41,10 @@ end
 def find_cool(cool)
   cool.select {|i| i.any? {|key, val| val == "cool"}}
 end
+
+def organized_schools(schools)
+  cities = {}
+  schools.each {|k, v| cities[v[:location]] = []}
+  cities.each {|k, v| schools.each {|k, v| if k = v1[:location]
+    then v << k1 end}}
+end
